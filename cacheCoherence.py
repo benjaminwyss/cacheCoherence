1e72d94b8f7c1c529252425b4a65db473dae973a
benjaminwyss <wyssbenjamin17@gmail.com>
2019-04-28 15:13:32 -0500

    Finish prerequestites for cache simulation.

--- cacheCoherence.py
+++ cacheCoherence.py
@@ -4,9 +4,14 @@ class CacheRecord:
         self.processor = processor
         self.isWrite = isWrite
         self.address = address
+        self.offset = int(address[30:32], 2)
+        # Number of lines = 32KB cache / 64B cache line = 9 bits of index
+        self.index = int(address[21:30], 2)
+        # 32 bit address - 2 bit offset - 9 bit index = 21 bits of tag
+        self.tag = int(address[0:21], 2)
     
     def __str__(self):
-        return "[{}, {}, {}, {}]".format(self.cycle, self.processor, ("Write" if self.isWrite else "Read"), self.address)
+        return "[{}, {}, {}, {}, {}, {}, {}]".format(self.cycle, self.processor, ("Write" if self.isWrite else "Read"), self.address, self.offset, self.index, self.tag)
 
     def __lt__(self, other):
         if (self.cycle < other.cycle):
@@ -16,24 +21,89 @@ class CacheRecord:
         else:
             return (self.processor < other.processor)
 
+class CacheLine:
+    def __init__(self):
+        self.tags = [False, False]
+        self.lru = 0
+        self.processorStates = ['I', 'I', 'I', 'I']
+
+    def __str__(self):
+        return "[{}, {}, {}]".format(self.tags, self.lru, self.processorStates)
+
+class processorStats:
+    def __init__(self):
+        self.cacheTransfers = [0, 0, 0, 0]
+        self.invalidationFromM = 0
+        self.invalidationFromO = 0
+        self.invalidationFromE = 0
+        self.invalidationFromS = 0
+        self.invalidationFromI = 0
+        self.dirtyWriteBacks = 0
+
 class CacheCoherence:
     def __init__(self):
         self.cacheRecords = []
+        # Since index is 9 bits, there are 2^9, or 512 cache lines
+        self.cacheLines = [CacheLine()] * 512
+        # Keep track of stats for each processor
+        self.processorStatTracker = [processorStats()] * 4
+
 
     def readCacheRecordsFromFiles(self):
         for i in range(0, 4):
             file = open(("p{}.tr".format(i)), "r")
             for line in file:
                 line = line.split()
-                cacheRecord = CacheRecord(int(line[0]), i, bool(int((line[1]))), line[2])
+                #line[0] is the clock cycle, i is the processor number, line[1] is a write/read flag, and line[2] is the memory adress, which is converted to binary and filled to 32 bits
+                cacheRecord = CacheRecord(int(line[0]), i, bool(int((line[1]))), bin(int(line[2], 0))[2:].zfill(32))
                 self.cacheRecords.append(cacheRecord)
-        
-        self.cacheRecords.sort()
+
+            # Based on the definition for sorting CacheRecords, this arranges the records in chronological order
+            self.cacheRecords.sort()
+    
+    def testPrints(self):
         for record in self.cacheRecords:
             print(record)
+        for cacheLine in self.cacheLines:
+            print(cacheLine)
+
+    def printStats(self):
+        print("P0 cache transfers: <p0-p1> = {}, <p0-p2> = {}, <p0-p3> = {}".format(self.processorStatTracker[0].cacheTransfers[1], self.processorStatTracker[0].cacheTransfers[2], self.processorStatTracker[0].cacheTransfers[3]))
+        print("P1 cache transfers: <p1-p0> = {}, <p1-p2> = {}, <p1-p3> = {}".format(self.processorStatTracker[1].cacheTransfers[0], self.processorStatTracker[1].cacheTransfers[2], self.processorStatTracker[1].cacheTransfers[3]))
+        print("P2 cache transfers: <p2-p0> = {}, <p2-p1> = {}, <p2-p3> = {}".format(self.processorStatTracker[2].cacheTransfers[0], self.processorStatTracker[2].cacheTransfers[1], self.processorStatTracker[2].cacheTransfers[3]))
+        print("P3 cache transfers: <p3-p0> = {}, <p3-p1> = {}, <p3-p2> = {}".format(self.processorStatTracker[3].cacheTransfers[0], self.processorStatTracker[3].cacheTransfers[1], self.processorStatTracker[3].cacheTransfers[2]))
+        print()
+
+        for processor in range(0, 4):
+            print("P{} Invalidation from: m = {}, o = {}, e = {}, s = {}, i = {}".format(processor, self.processorStatTracker[processor].invalidationFromM, self.processorStatTracker[processor].invalidationFromO, self.processorStatTracker[processor].invalidationFromE, self.processorStatTracker[processor].invalidationFromS, self.processorStatTracker[processor].invalidationFromI))
+        print()
+
+        print("Dirty Writebacks: P0 = {}, P1 = {}, P2 = {}, P3 = {}".format(self.processorStatTracker[0].dirtyWriteBacks, self.processorStatTracker[1].dirtyWriteBacks, self.processorStatTracker[2].dirtyWriteBacks, self.processorStatTracker[3].dirtyWriteBacks))
+        print()
+
+        for processor in range(0, 4):
+            m = 0
+            o = 0
+            e = 0
+            s = 0
+            i = 0
+            for cacheLine in self.cacheLines:
+                if cacheLine.processorStates[processor] == 'M':
+                    m += 1
+                elif cacheLine.processorStates[processor] == 'O':
+                    o += 1
+                elif cacheLine.processorStates[processor] == 'E':
+                    e += 1
+                elif cacheLine.processorStates[processor] == 'S':
+                    s += 1
+                elif cacheLine.processorStates[processor] == 'I':
+                    i += 1
+            print("Final States for P{}: m = {}, o = {}, e = {}, s = {}, i = {}".format(processor, m, o, e, s, i))
 
 
 
 if __name__ == "__main__":
     myCacheCoherence = CacheCoherence()
     myCacheCoherence.readCacheRecordsFromFiles()
+    myCacheCoherence.testPrints()
+    myCacheCoherence.printStats()
